be5f7fde87a6996c5398036bd3e3b97993114cb0
Marcin Wosinek <marcin@wosinek.eu>
2021-08-12 20:29:32 +0200

    add simple build script

---
+++ build.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log("test");
+
+require("esbuild")
+  .build({
+    logLevel: "info",
+    entryPoints: ["src/index.js"],
+    bundle: true,
+    outfile: "www/main.js",
+  })
+  .catch(() => process.exit(1));
